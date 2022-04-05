bb9b163e53a8393d343535473b3d806dee367b46
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-04-05 21:26:16 +0300

    fixed AboutInheritance.js

--- koans/AboutInheritance.js
+++ koans/AboutInheritance.js
@@ -1,44 +1,48 @@
-function Muppet(age, hobby) {
-  this.age = age;
-  this.hobby = hobby;
+class Muppet {
+  constructor(age, hobby) {
+    this.age = age;
+    this.hobby = hobby;
 
-  this.answerNanny = function(){
-	return "Everything's cool!";
+    this.answerNanny = function () {
+      return "Everything's cool!";
+    };
   }
 }
 
-function SwedishChef(age, hobby, mood) {
-  Muppet.call(this, age, hobby);
-  this.mood = mood;
+class SwedishChef extends Muppet {
+  constructor(age, hobby, mood) {
+    super(age, hobby);
+    this.mood = mood;
 
-  this.cook = function() {
-    return "Mmmm soup!";
+    this.cook = function () {
+      return "Mmmm soup!";
+    };
   }
 }
 
-SwedishChef.prototype = new Muppet();
+// SwedishChef.prototype = new Muppet();
 
 describe("About inheritance", function() {
   beforeEach(function(){
     this.muppet = new Muppet(2, "coding");
-	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
+    this.swedishChef = new SwedishChef(2, "cooking", "chillin");
   });
 
   it("should be able to call a method on the derived object", function() {
-    expect(this.swedishChef.cook()).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.cook()).toEqual("Mmmm soup!");
   });
 
   it("should be able to call a method on the base object", function() {
-    expect(this.swedishChef.answerNanny()).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.answerNanny()).toEqual("Everything's cool!");
   });
 
   it("should set constructor parameters on the base object", function() {
-    expect(this.swedishChef.age).toEqual(FILL_ME_IN);
-    expect(this.swedishChef.hobby).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.age).toEqual(2);
+    expect(this.swedishChef.hobby).toEqual("cooking");
   });
 
   it("should set constructor parameters on the derived object", function() {
-    expect(this.swedishChef.mood).toEqual(FILL_ME_IN);
+    expect(this.swedishChef.mood).toEqual("chillin");
   });
 });
 
@@ -49,17 +53,19 @@ Object.prototype.beget = function () {
   return new F();
 }
 
-function Gonzo(age, hobby, trick) {
-  Muppet.call(this, age, hobby);
-  this.trick = trick;
+class Gonzo extends Muppet {
+  constructor(age, hobby, trick) {
+    super(age, hobby);
+    this.trick = trick;
 
-  this.doTrick = function() {
-    return this.trick;
+    this.doTrick = function () {
+      return this.trick;
+    };
   }
 }
 
 //no longer need to call the Muppet (base type) constructor
-Gonzo.prototype = Muppet.prototype.beget();
+// Gonzo.prototype = Muppet.prototype.beget();
 
 describe("About Crockford's inheritance improvement", function() {
   beforeEach(function(){
@@ -67,19 +73,19 @@ describe("About Crockford's inheritance improvement", function() {
   });
 
   it("should be able to call a method on the derived object", function() {
-    expect(this.gonzo.doTrick()).toEqual(FILL_ME_IN);
+    expect(this.gonzo.doTrick()).toEqual("eat a tire");
   });
 
   it("should be able to call a method on the base object", function() {
-    expect(this.gonzo.answerNanny()).toEqual(FILL_ME_IN);
+    expect(this.gonzo.answerNanny()).toEqual("Everything's cool!");
   });
 
   it("should set constructor parameters on the base object", function() {
-    expect(this.gonzo.age).toEqual(FILL_ME_IN);
-    expect(this.gonzo.hobby).toEqual(FILL_ME_IN);
+    expect(this.gonzo.age).toEqual(3);
+    expect(this.gonzo.hobby).toEqual("daredevil performer");
   });
 
   it("should set constructor parameters on the derived object", function() {
-    expect(this.gonzo.trick).toEqual(FILL_ME_IN);
+    expect(this.gonzo.trick).toEqual("eat a tire");
   });
 });
